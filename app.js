601b7c8bfa67601ecdec2faed0511f246290f5b9
unknown <unknown@example.com>
2019-01-07 00:00:57 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,56 @@
+//app.js
+var api = require('/api/api.js')
+var AppId = 'wxf49a2a5f112d8629'
+var AppSecret = '4cc746bae6186acaebdcccac58d42522'
+
+App({
+  onLaunch: function () {
+  },
+  getUserInfo: function (cb) {
+    var that = this
+    //调用登录接口，获取 code
+    wx.login({
+      success: function (res) {
+        //发起网络请求
+        wx.request({
+          url: api.mobileIn,
+          data: {
+            appid: AppId,
+            secret: AppSecret,
+            code: res.code,
+            grant_type: 'authorization_code'
+          },
+          header: {
+            method: 'GET_OPENID'
+          },
+          method: 'GET',
+          success: function (res) {
+            // 将openId设成全局
+            that.globalData.openId = res.data.openid
+            console.log('res.data.openid', res.data.openid)
+            that.globalData.flag = res.data.flag
+            wx.getUserInfo({
+              success: function (res) {
+                // 将userInfo设成全局
+                that.globalData.userInfo = res.userInfo
+                typeof cb == "function" && cb(that.globalData.userInfo, that.globalData.openId, that.globalData.user, that.globalData.flag)
+              }
+            })
+          },
+          fail: function (res) { },
+          complete: function (res) { 
+          }
+        });
+      }
+    })
+  },
+  globalData: {
+    userInfo: null,
+    openId: null,
+    user: null,
+    flag: null,
+    hostUserId:null,
+    isOfficial: true,
+    isCreate: true
+  }
+})
